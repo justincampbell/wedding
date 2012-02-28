aea2a640a6494de5cdd27ac641fc3df7138916e5
Justin Campbell <justin@justincampbell.me>
2012-02-27 23:01:28 -0500

    Do no initialize during asset precompilation

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Wedding
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Fix for Heroku + NewRelic and asset compilation
+    config.assets.initialize_on_precompile = false
   end
 end
