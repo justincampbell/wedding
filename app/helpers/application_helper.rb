1d3068b6dd78a9e5566cfedc0d4da46c70ae3413
Justin Campbell <justin@justincampbell.me>
2012-07-08 15:35:04 -0400

    Update page structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def registry_link(id, name, &block)
+    urls = {
+      bed_bath_and_beyond: "http://www.bedbathandbeyond.com/regGiftRegistry.asp?wrn=%2D347647309",
+      crate_and_barrel: "http://www.crateandbarrel.com/Gift-Registry/Guest/View-Registry.aspx?grid=4893046"
+    }
+
+    link_to urls[id], title: name, target: :_blank, &block
+  end
 end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -2,6 +2,22 @@ When /^I visit the site$/ do
   visit root_path
 end
 
-Then /^I should see (.+)$/ do |text|
+When /^(?:|I )click "(.+)"$/ do |link|
+  click_link link
+end
+
+Then /^(?:|I )should see paragraphs$/ do
+  page.should have_selector('p')
+end
+
+Then /^(?:|I )should see "(.+)"$/ do |text|
   page.should have_content(text)
+end
+
+Then /^(?:|I )should see (?:a|an) "(.+)" section$/ do |section|
+  page.should have_content(section)
+end
+
+Then /show me the page/ do
+  save_and_open_page
 end
