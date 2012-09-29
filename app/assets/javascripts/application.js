a2c82f0b21faa620143a109e744863863e55c0b7
Ashton Harris <ahdesignsonline@gmail.com>
2012-09-29 02:56:21 -0400

    starts redesign of full site
    *updates css of layout
    *adds html5 tags to layout view
    *adds JS to toggle menu visibility

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// $().ready{
+//   toggleMenu();
+// }
+
+
+$('.menu-button a').click(function(){
+ $('.menu ul').toggleClass('expanded');
+});
