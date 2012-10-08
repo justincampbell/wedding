3e8f2590b497c6b5a3c79aedce4d5ffd360c06d7
Justin Campbell <justin@justincampbell.me>
2012-10-07 20:33:52 -0400

    Weaken guest validations, do no automatically create party

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,7 +2,8 @@ class Guest < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :party, :zip_code
   attr_accessible :rsvp_attributes
 
-  validates_presence_of :first_name, :last_name, :zip_code
+  validates_presence_of :first_name, :zip_code
+  validates_uniqueness_of :first_name, scope: [:last_name, :zip_code]
 
   belongs_to :party
   has_one :rsvp
@@ -11,7 +12,6 @@ class Guest < ActiveRecord::Base
 
   store :settings, accessors: [:invited_to_rehearsal_dinner]
 
-  before_create -> { self.party ||= Party.create name: last_name }
   after_initialize -> { self.rsvp ||= Rsvp.new }
 
   def full_name
