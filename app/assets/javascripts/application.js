d04c1f1d25b6eaa56c580ab09110bc820a99f6d8
Justin Campbell <justin@justincampbell.me>
2012-10-10 21:54:36 -0700

    Toggle only current guest, fix toggle state
    [fix #21]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,7 +31,6 @@ function setContentHeight(){
   $('.content-wrap').css("min-height", ($(window).height() + "px" ));
 }
 
-$('#party_guests_attributes_0_rsvp_attributes_attending').click(function(){
-    var chk = $(this);
-    $(this).next($('.attending-content').toggleClass('expanded', chk.attr('checked')));
+$('.attending-checkbox').click(function() {
+  $(this).parent().siblings('.attending-content').toggleClass('expanded', !!$(this).attr('checked'));
 })
